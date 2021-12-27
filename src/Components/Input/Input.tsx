e671ecd904e45d53bf2d335a9ffb0ad4eaea32d4
farzaneh <farzaneh.kamali@gmail.com>
2021-12-27 18:36:55 +0330

    ✨ Add Input and Button Component

---
+++ src/Components/Input/Input.tsx
@@ -0,0 +1,64 @@
+import { Field } from "formik";
+import React from "react";
+
+interface StringInputProps {
+  name: string;
+  placeholder: string;
+  dir: string;
+}
+
+export const StringInput = (props: StringInputProps) => {
+  return (
+    <div className={"String_Input"}>
+      <Field
+        type="text"
+        dir={props.dir}
+        placeholder={props.placeholder}
+        id={props.name}
+        name={props.name}
+      />
+    </div>
+  );
+};
+
+const ChoiceYearInput = (props: { name: string }) => {
+  const years = [];
+  for (let year = 1340; year < 1400; year++) {
+    years.push(<option value={year}>{year}</option>);
+  }
+  return (
+    <div className={"choice_input input_time_year"}>
+      <Field as="select" name={props.name} className={"field_choice_input"}>
+        {years}
+      </Field>
+    </div>
+  );
+};
+
+const ChoiceMonthInput = (props: { name: string }) => {
+  return (
+    <div className={"choice_input input_time_month"}>
+      <Field as="select" name={props.name} className={"field_choice_input"}>
+        <option value="فروردین">فروردین</option>
+        <option value="اردیبهشت">اردیبهشت</option>
+        <option value="خرداد">خرداد</option>
+        <option value="تیر">تیر</option>
+        <option value="مرداد">مرداد</option>
+        <option value="شهریور">شهریور</option>
+        <option value="دی">دی</option>
+        <option value="بهمن">بهمن</option>
+        <option value="اسفند">اسفند</option>
+      </Field>
+    </div>
+  );
+};
+
+export const ChoiceTimeInput = (props: { name: string; id: string }) => {
+  return (
+    <div className={"time_input"}>
+      <span className={"input_time_title"}>{props.name}</span>
+      <ChoiceYearInput name={props.id + "Year"} />
+      <ChoiceMonthInput name={props.id + "Month"} />
+    </div>
+  );
+};
--- src/Pages/Profile/UserProfile/UserProfile.tsx
+++ src/Pages/Profile/UserProfile/UserProfile.tsx
@@ -1,6 +1,8 @@
 import React from "react";
-import { Formik, Form, FormikHelpers } from "formik";
+import { Formik, Form } from "formik";
 import { InitCard } from "../../../Components/InitCard/InitCard";
+import { ChoiceTimeInput, StringInput } from "../../../Components/Input/Input";
+import { Button } from "../../../Components/Button/Button";
 
 const initialValues = {
   firstName: "",
@@ -33,41 +35,51 @@ export const UserProfile = () => {
     console.log(values);
   };
   return (
-    <div>
+    <div className="Card_Container">
       <InitCard>
         <Formik initialValues={initialValues} onSubmit={onSubmit}>
-          <Form>
-            <StringInput placeholder={"نام"} name={"firstName"} dir={"rtl"} />
-            <StringInput
-              placeholder={"نام خانوادگی"}
-              name={"lastName"}
-              dir={"rtl"}
-            />
-            <StringInput
-              placeholder={"نام کاربری"}
-              name={"username"}
-              dir={"rtl"}
-            />
-            <StringInput
-              placeholder={"توضیحات"}
-              name={"description"}
-              dir={"rtl"}
-            />
-            <StringInput
-              placeholder={"آخرین عنوان شغلی"}
-              name={"title"}
-              dir={"rtl"}
-            />
-            <StringInput
-              placeholder={"نام شرکت"}
-              name={"company"}
-              dir={"rtl"}
-            />
-            <ChoiceTimeInput name={"تاریخ شروع"} id={"startedAt"} />
-            <ChoiceTimeInput name={"تاریخ پایان"} id={"finishedAt"} />
-            {/*<PictureInput name={"+ اضافه کردن تصویر پروفایل"}/>*/}
-            <ButtonPrimary name={"ثبت"} />
-          </Form>
+          {(formik) => {
+            <Form>
+              <StringInput placeholder={"نام"} name={"firstName"} dir={"rtl"} />
+              <StringInput
+                placeholder={"نام خانوادگی"}
+                name={"lastName"}
+                dir={"rtl"}
+              />
+              <StringInput
+                placeholder={"نام کاربری"}
+                name={"username"}
+                dir={"rtl"}
+              />
+              <StringInput
+                placeholder={"توضیحات"}
+                name={"description"}
+                dir={"rtl"}
+              />
+              <StringInput
+                placeholder={"آخرین عنوان شغلی"}
+                name={"title"}
+                dir={"rtl"}
+              />
+              <StringInput
+                placeholder={"نام شرکت"}
+                name={"company"}
+                dir={"rtl"}
+              />
+              <ChoiceTimeInput name={"تاریخ شروع"} id={"startedAt"} />
+              <ChoiceTimeInput name={"تاریخ پایان"} id={"finishedAt"} />
+              {/*<PictureInput name={"+ اضافه کردن تصویر پروفایل"}/>*/}
+              <Button
+                type="submit"
+                gruop="Primary"
+                lang="fa"
+                size="XXL"
+                disabled={!formik.dirty}
+              >
+                ثبت
+              </Button>
+            </Form>;
+          }}
         </Formik>
       </InitCard>
     </div>
